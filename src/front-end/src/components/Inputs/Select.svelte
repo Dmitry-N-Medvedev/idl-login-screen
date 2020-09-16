044006bd29b9c85a11691763c1512f885c97856e
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2020-09-16 19:59:20 +0200

    WIP: input select
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

---
+++ src/front-end/src/components/Inputs/Select.svelte
@@ -0,0 +1,40 @@
+<script>
+  import ControlContainer from '../ControlContainer.svelte';
+  import {
+    generateComponentId,
+  } from '../helpers/generateComponentId.mjs';
+
+  export let options;
+
+  let datalist;
+
+  $: if (options) {
+    datalist = options.split(',');
+  }
+</script>
+<style>
+  .selectInput {
+    position: relative;
+    display: flex;
+    flex: 1 0 auto;
+    border: none;
+  }
+
+  .selectInput::placeholder {
+    text-transform: capitalize;
+  }
+
+  .selectInput:focus {
+    outline: none;
+  }
+</style>
+
+<ControlContainer>
+  {#if datalist}
+    <select {...$$props} {...$$restProps} class="selectInput">
+      {#each datalist as option}
+        <option value={option}>{option.toUpperCase()}</option>
+      {/each}
+    </select>
+  {/if}
+</ControlContainer>
