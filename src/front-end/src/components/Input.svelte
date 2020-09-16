997a594cb4e01963524878c2826e68fa35f2d597
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2020-09-16 18:08:50 +0200

    WIP: input date
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

--- src/front-end/src/components/Input.svelte
+++ src/front-end/src/components/Input.svelte
@@ -2,17 +2,19 @@
   // import Icon from 'fa-svelte';
   // import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons/faCalendarAlt.js';
   import InputText from './Inputs/Text.svelte';
-  import Checkbox from './Inputs/Checkbox.svelte';
-  import Radio from './Inputs/Radio.svelte';
+  import InputCheckbox from './Inputs/Checkbox.svelte';
+  import InputRadio from './Inputs/Radio.svelte';
+  import InputDate from './Inputs/Date.svelte';
 
   import {
     generateComponentId,
   } from './helpers/generateComponentId.mjs';
 
   const components = {
-    checkbox: Checkbox,
+    checkbox: InputCheckbox,
     text: InputText,
-    radio: Radio,
+    radio: InputRadio,
+    date: InputDate,
   };
 </script>
 
