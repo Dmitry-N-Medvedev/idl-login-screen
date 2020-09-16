1ee724eb4ec73437db8b86305e06c10e69d3b1ff
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2020-09-16 15:09:51 +0200

    WIP: implementing individual input components
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

---
+++ src/front-end/src/components/Inputs/Checkbox.svelte
@@ -0,0 +1,7 @@
+<script>
+import AuthForm from "../AuthForm.svelte";
+import CircularImageButton from "../CircularImageButton.svelte";
+
+</script>
+<input type="checkbox" {...$$props} {...$$restProps} />
+<label for={$$restProps['id']}>some checkbox</label>
