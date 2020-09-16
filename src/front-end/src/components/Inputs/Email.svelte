c31577cb2521caf58d56846828bacaeb9b82bdcc
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2020-09-16 18:38:43 +0200

    WIP: input email
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

---
+++ src/front-end/src/components/Inputs/Email.svelte
@@ -0,0 +1,23 @@
+<script>
+  import ControlContainer from '../ControlContainer.svelte';
+</script>
+<style>
+  .emailInput {
+    position: relative;
+    display: flex;
+    flex: 1 0 auto;
+    border: none;
+  }
+
+  .emailInput::placeholder {
+    text-transform: capitalize;
+  }
+
+  .emailInput:focus {
+    outline: none;
+  }
+</style>
+
+<ControlContainer>
+  <input class="emailInput" type="email" {...$$props} {...$$restProps} />
+</ControlContainer>
