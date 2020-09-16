43b0e0283c29c07ac4f97d22c264b0176d8cf29a
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2020-09-16 18:08:50 +0200

    WIP: input date
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

---
+++ src/front-end/src/components/Inputs/Date.svelte
@@ -0,0 +1,28 @@
+<script>
+  // import Icon from 'fa-svelte';
+  // import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons/faCalendarAlt.js';
+  import ControlContainer from '../ControlContainer.svelte';
+</script>
+
+<style>
+  .inputDate {
+    position: relative;
+    display: flex;
+    flex: 1 0 auto;
+    border: none;
+
+    cursor: pointer;
+  }
+
+  .inputDate::-webkit-calendar-picker-indicator {
+    cursor: pointer;
+  }
+
+  .inputDate:focus {
+    outline: none;
+  }
+</style>
+
+<ControlContainer>
+  <input type="date" {...$$props} {...$$restProps} class="inputDate" />
+</ControlContainer>
