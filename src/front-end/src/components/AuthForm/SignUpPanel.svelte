90dc7517672b367719eb41489d24837dd3ab9a49
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2020-09-17 11:34:45 +0200

    WIP: SignUpPanel
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

--- src/front-end/src/components/AuthForm/SignUpPanel.svelte
+++ src/front-end/src/components/AuthForm/SignUpPanel.svelte
@@ -1,3 +1,22 @@
-<div>
+<style>
+  #sign-up-screen {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 1fr 1fr 5fr 2fr 2fr;
+    grid-template-areas:
+      'sign-up-social'
+      'sign-up-devider'
+      'sign-up-panel'
+      'sign-up-checkboxes'
+      'sign-up-button'
+    ;
+
+    /* background-color: var(--auth-form-background-color); */
+    width: 100%;
+    height: 100%;
+  }
+</style>
+
+<article id="sign-up-screen">
   SignUpPanel
-</div>
+</article>
