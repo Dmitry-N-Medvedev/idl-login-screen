7ba9149baab6e13c0e80fbe117850c001230920f
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2020-09-20 21:19:07 +0200

    rm unnecessary reference
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

--- src/front-end/src/fsm/ServerCommunicator.fsm.mjs
+++ src/front-end/src/fsm/ServerCommunicator.fsm.mjs
@@ -4,14 +4,18 @@ import {
 import {
   BroadcastChannelNames,
 } from '../constants/BroadcastChannelNames.mjs';
+// import {
+//   MessageTypes,
+// } from '../../../common/messages/MessageTypes.mjs';
 import {
-  StructureTypes,
-} from '../constants/StructureTypes.mjs';
+  WebSocketsCloseCodes,
+} from '../../../common/constants/WebSocketsCloseCodes.mjs';
 import {
   serializeMessage,
-} from './SerDe/serializeMessage.mjs';
+} from '../../../common/SerDe/serializeMessage.mjs';
 
 let serverCommunicatorBroadcastChannel = null;
+let client = null;
 
 const handleServerCommunicatorMessage = async ({ data: { type, payload } }) => {
   try {
@@ -19,26 +23,76 @@ const handleServerCommunicatorMessage = async ({ data: { type, payload } }) => {
       type,
       payload,
     }));
-  
-    console.debug('[ServerCommunicator.fsm] send serializedMessage to the server', serializedMessage);
+
+    client.send(serializedMessage);
   } catch (serializationError) {
+    // eslint-disable-next-line no-console
     console.debug(serializationError);
   }
 };
 
-const start = () => {
-  serverCommunicatorBroadcastChannel = new BroadcastChannel(BroadcastChannelNames.ServerCommunicatorBroadcastChannel);
+const startClient = () => new Promise((resolve, reject) => {
+  try {
+    client = new WebSocket('ws://localhost:9001/');
+
+    client.onerror = async (event) => {
+      // eslint-disable-next-line no-console
+      console.debug('client.onerror:', event);
+
+      await startClient();
+    };
+
+    client.onopen = () => {
+      resolve();
+    };
+
+    client.onclose = (event) => {
+      // eslint-disable-next-line no-console
+      console.debug('client.onclose:', event);
+    };
+
+    client.onmessage = async ({ data }) => {
+      const message = JSON.parse((new TextDecoder()).decode(await data.arrayBuffer()));
+
+      // eslint-disable-next-line no-console
+      console.debug('client.onmessage:', message);
+    };
+  } catch (error) {
+    reject(error);
+  }
+});
+
+const stopClient = () => new Promise((resolve, reject) => {
+  try {
+    client.close(WebSocketsCloseCodes.CLOSE_NORMAL, 'bye :)', resolve);
+  } catch (error) {
+    reject(error);
+  } finally {
+    client = null;
+  }
+});
+
+const start = async () => {
+  serverCommunicatorBroadcastChannel = new BroadcastChannel(
+    BroadcastChannelNames.ServerCommunicatorBroadcastChannel,
+  );
   serverCommunicatorBroadcastChannel.onmessage = handleServerCommunicatorMessage;
 
+  await startClient();
+
+  // eslint-disable-next-line no-restricted-globals
   self.postMessage(FsmProtocolMessages.started);
 };
 
-const stop = () => {
+const stop = async () => {
   if (serverCommunicatorBroadcastChannel) {
     serverCommunicatorBroadcastChannel.onmessage = null;
     serverCommunicatorBroadcastChannel.stop();
   }
 
+  await stopClient();
+
+  // eslint-disable-next-line no-restricted-globals
   self.postMessage(FsmProtocolMessages.stopped);
 };
 
@@ -51,17 +105,21 @@ const handleIncomingMessage = (message) => {
       return stop();
     }
     default: {
+      // eslint-disable-next-line no-console
       console.error('unknown message type', message);
     }
   }
+
+  return undefined;
 };
 
-onmessage = function ({
-  data
-}) {
+// eslint-disable-next-line func-names
+onmessage = function ({ data }) {
   handleIncomingMessage(data);
 };
 
+// eslint-disable-next-line no-restricted-globals, func-names
 onerror = function (event) {
+  // eslint-disable-next-line no-console
   console.debug('ServerCommunicator.fsm.onerror', event);
 };
